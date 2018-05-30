0a10b8911e9838e6f216579d407e019a07469f47
Stevens <korwinstevens@quickenloans.com>
2018-05-30 10:03:21 -0400

    Clean up map and template to be more exact

--- telegraf.go
+++ telegraf.go
@@ -10,10 +10,11 @@ func telegraf(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		t, _ := template.ParseFiles("telegraf.html")
 
-		gT := make(map[string]string)
-		gT["env"] = r.PostFormValue("env")
-		gT["role"] = r.PostFormValue("role")
-		gT["app"] = r.PostFormValue("app")
+		gT := map[string]string{
+			"env": r.PostFormValue("env"),
+			"role": r.PostFormValue("role"),
+			"app": r.PostFormValue("app"),
+		}
 
 		t.Execute(w, gT)
 		
