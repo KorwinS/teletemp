824198e7f4e3183edb95f01e3ffb8e8cee132c5a
Stevens <korwinstevens@quickenloans.com>
2018-05-30 12:00:59 -0400

    cleanup telegraf.go and add linux and windows templates

--- telegraf.go
+++ telegraf.go
@@ -8,15 +8,20 @@ import (
 // Get the input from the form and genrate the tags
 func telegraf(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		t, _ := template.ParseFiles("telegraf.html")
-
 		gT := map[string]string{
 			"env": r.PostFormValue("env"),
 			"role": r.PostFormValue("role"),
 			"app": r.PostFormValue("app"),
 		}
-
-		t.Execute(w, gT)
+		if r.PostFormValue("hostType") == "Linux" {
+			t, _ := template.ParseFiles("telegraf-linux.html")
+			t.Execute(w, gT)
+	
+		} else {
+			t, _ := template.ParseFiles("telegraf-windows.html")
+			t.Execute(w, gT)
+	
+		}
 		
 	}
 }
